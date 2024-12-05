40fb47295eeea801ae3a21772e3eb8a344ee87b5
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-12-05 17:22:38 +0300

    adding a player API

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+import axios from "axios";
 import exfdgpress from "express";
 import * as exp from "express"
-import { application } from "express";
-const app = exp();
+var app = exfdgpress();
 
 
+function createPlayer(playerID) {
+    
+    axios.post("https://api.whalepass.gg/enrollments",{
+        "gameId":"2bcfa666-cae1-470e-ab2e-b249d66005f0",
+        "playerId":"0"
+    },{headers:{
+    
+        "X-API-KEY":process.env.API
+    }}).then((d)=>{
+    
+        console.log(d);
+        
+    })
+    // 
+    
+}
 app.listen(5000,()=>{
 
+    
 
     console.log("sever is up and running");
+    createPlayer()
     
 })
