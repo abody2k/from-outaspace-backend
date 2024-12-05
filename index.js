a41734fe3b7c24a7185e815c06b4110ece00826f
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-12-05 16:47:50 +0300

    setting up the backend

---
+++ index.js
@@ -0,0 +1,12 @@
+import exfdgpress from "express";
+import * as exp from "express"
+import { application } from "express";
+const app = exp();
+
+
+app.listen(5000,()=>{
+
+
+    console.log("sever is up and running");
+    
+})
