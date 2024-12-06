2b88aacc353720bc6fe02386d601f72bc12ebb7e
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-12-06 11:13:04 +0300

    added making new player restAPI, used a redis db to store the next ID and install jsonwebtoken package

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
 import axios from "axios";
 import exfdgpress from "express";
-import * as exp from "express"
+import { createClient } from "redis";
 var app = exfdgpress();
+app.use(exfdgpress.json())
 
 
-function createPlayer(playerID) {
+app.post("/cp",async (req,res)=>{
+
+
+
+    try {
+
+  
+          //read from redis the next playerID
+          const client =await createClient({
+            url:`redis://default:${process.env.REDIS}@redis-17431.c52.us-east-1-4.ec2.redns.redis-cloud.com:17431`
+        }).on('error',(e)=>{
+
+            console.log(e);
+            
+        }).connect();
+
+        const playerID=await client.get("nextPlayerId");
+
+        await createPlayer(Number(playerID));
+        console.log(`current player ID is ${playerID}`);
+        
+        await client.set("nextPlayerId",Number(playerID)+1);
+        console.log("Just made a player");
+        res.sendStatus(200);
+        
+
+    } catch (error) {//player already exists
+        res.sendStatus(403);
+    }
+
+
+
+
+
+});
+
+
+
+async function createPlayer(playerID) {
+
     
-    axios.post("https://api.whalepass.gg/enrollments",{
+    await axios.post("https://api.whalepass.gg/enrollments",{
         "gameId":"2bcfa666-cae1-470e-ab2e-b249d66005f0",
-        "playerId":"0"
+        "playerId":playerID
     },{headers:{
     
         "X-API-KEY":process.env.API
-    }}).then((d)=>{
-    
-        console.log(d);
-        
-    })
-    // 
+    }})
     
 }
-app.listen(5000,()=>{
+
+
+function getAPlayer(playerID){
+
+
+
+}
+app.listen(5000,async()=>{
 
     
+console.log("Server is up and running");
 
-    console.log("sever is up and running");
-    createPlayer()
+
     
 })
